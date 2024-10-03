97727eb900c752c75546d507e1f4212ec064ec07
modest-azad <colleizian@gmail.com>
2024-10-03 15:05:22 +0530

    logo added and color changed

--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -10,20 +10,20 @@ function LoginForm({ loading }) {
         method="post"
       >
         <h1 className="font-bold text-lg text-neutral-900 mb-5">
-          Login to Your Account
+          Login to TechKshitiz Quiz
         </h1>
         <label
           className="text-xs md:text-sm text-neutral-600 font-semibold mb-2"
           htmlFor="email"
         >
-          Email
+          Techkshitiz ID
         </label>
         <input
           type="email"
           autoComplete="off"
           name="email"
-          placeholder="Enter your email"
-          className="bg-neutral-50 ring-[1px] ring-gray-200 rounded-lg p-2 md:p-3 placeholder:text-gray-300 text-xs md:text-sm focus:border-none placeholder:text-xs md:placeholder:text-sm focus:outline-none focus:ring-[1px] focus:ring-orange-500/80 "
+          placeholder="Enter your ID"
+          className="bg-neutral-50 ring-[1px] ring-gray-200 rounded-lg p-2 md:p-3 placeholder:text-gray-300 text-xs md:text-sm focus:border-none placeholder:text-xs md:placeholder:text-sm focus:outline-none focus:ring-[1px] focus:ring-sky-500/80 "
         />{" "}
         <br />
         <div className="flex justify-between items-center mb-2">
@@ -36,7 +36,7 @@ function LoginForm({ loading }) {
         </div>
         <input
           type="password"
-          className="bg-neutral-50 ring-[1px] ring-gray-200 rounded-lg p-2 md:p-3 placeholder:text-gray-300 text-xs md:text-sm focus:border-none placeholder:text-xs md:placeholder:text-sm focus:outline-none focus:ring-[1px] focus:ring-orange-500/80"
+          className="bg-neutral-50 ring-[1px] ring-gray-200 rounded-lg p-2 md:p-3 placeholder:text-gray-300 text-xs md:text-sm focus:border-none placeholder:text-xs md:placeholder:text-sm focus:outline-none focus:ring-[1px] focus:ring-sky-500/80"
           name="password"
           placeholder="Enter your password"
         />
@@ -44,8 +44,8 @@ function LoginForm({ loading }) {
         <button
           disabled={loading}
           className={`flex rounded-full ${!loading
-              ? "bg-orange-500 hover:bg-neutral-50 hover:text-orange-500"
-              : "bg-orange-500/70 cursor-not-allowed"
+              ? "bg-sky-500 hover:bg-neutral-50 hover:text-sky-500"
+              : "bg-sky-500/70 cursor-not-allowed"
             }  p-1 justify-center font-semibold md:font-bold text-base md:text-lg text-center  mb-3 transition text-white`}
           type="submit"
         >
@@ -66,7 +66,7 @@ function LoginForm({ loading }) {
         </button>
         <p className="text-gray-600 text-xs font-semibold mt-3">
           Don't have account ?
-          <Link className="ml-2 text-orange-500" to={"/register"}>
+          <Link className="ml-2 text-sky-500" to={"/register"}>
             Sign Up
           </Link>{" "}
         </p>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,15 +5,22 @@ function Navbar() {
   const { auth, logoutUser } = useQuestionStore();
 
   return (
-    <nav className="w-full bg-transparent text-neutral-900 px-5 md:px-10 flex items-center justify-between py-5 text-sm border-b-slate-200 border">
-      <h1 className="text-orange-500 text-2xl font-bold tracking-tight flex items-center gap-1">
-        Bert<span className="text-neutral-900"> Quiz</span>
+    <nav className="w-full bg-transparent text-neutral-900 px-5 py-5 md:px-10 flex items-center justify-between text-sm border-b-slate-200 border">
+      {/* website logo  */}
+      <div className="logo h-[50px] flex content-center ">
+        <img
+          className="h-full"
+          src="/techkshitiz/techkshitizLogo.svg"
+          alt="techkshitiz logo"
+        />
+      </div>
+      <h1 className="text-sky-500 text-2xl font-bold tracking-tight flex items-center gap-1">
         {/* <div className="w-2 h-2 bg-red-500 mt-2 ml-1 rounded-full" /> */}
       </h1>
       <div className="space-x-5">
         {!auth?.email && (
           <Link
-            className="py-2 px-5 text-orange-500 font-semibold border rounded-full border-orange-500"
+            className="py-2 px-5 text-sky-500 font-semibold border rounded-full border-sky-500"
             to={"/login"}
           >
             Login
@@ -28,7 +35,7 @@ function Navbar() {
           </button>
         ) : (
           <Link
-            className="py-2 transition px-5 text-neutral-50 font-semibold rounded-full bg-orange-500"
+            className="py-2 transition px-5 text-neutral-50 font-semibold rounded-full bg-sky-500"
             to={"/register"}
           >
             Register
